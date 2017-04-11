de5503444e4fc33a3a86d21421999b8e7df7c840
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-04-11 15:23:09 +0200

    debug

--- lead/list.php
+++ lead/list.php
@@ -327,7 +327,7 @@ if ($resql != - 1) {
 	$i = 0;
 	print '<table class="noborder" width="100%">';
 	print '<tr class="liste_titre">';
-	print '<td align="center" style="width: 95px;">Action</td>';
+	print '<td class="liste_titre" align="center">Action</td>';
 	print_liste_field_titre($langs->trans("Ref"), $_SERVEUR['PHP_SELF'], "t.ref", "", $option, '', $sortfield, $sortorder);
 	print_liste_field_titre($langs->trans("LeadCommercial"), $_SERVEUR['PHP_SELF'], "usr.lastname", "", $option, '', $sortfield, $sortorder);
 	print_liste_field_titre($langs->trans("Customer"), $_SERVEUR['PHP_SELF'], "so.nom", "", $option, '', $sortfield, $sortorder);
@@ -438,7 +438,7 @@ if ($resql != - 1) {
 		$var = ! $var;
 		print '<tr ' . $bc[$var] . '>';
 
-		print '<td align="center">' . $list . '</td>';
+		print '<td align="center" style="display: inline">' . $list . '</td>';
 
 		// Ref
 		print '<td><a href="card.php?id=' . $line->id . '">' . $line->ref . '</a>';
