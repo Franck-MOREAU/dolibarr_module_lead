7eb9ce61988b6eccf2fc207c4b2926c00e6d3b2d
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-04-11 18:21:30 +0200

    debug

--- lead/lead_portfolio.php
+++ lead/lead_portfolio.php
@@ -79,7 +79,7 @@ if ($resql != - 1) {
     print ' $.ajax({';
     print 'method: "POST",';
     print 'url: "dragdrop.php",';
-    print 'data: { nom: ev.target.id, org: ev.caller.id }';
+    print 'data: { nom: ev.target.id, org: caller.id }';
   	print '})';
   	print '.done(function(msg) {';
    	print 'alert( "Data Saved: " + msg );';
