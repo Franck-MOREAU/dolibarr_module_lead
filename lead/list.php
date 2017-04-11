f98325dd346f488e0b350629cf9dbb33a80d76af
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-04-11 15:13:05 +0200

    debug

--- lead/list.php
+++ lead/list.php
@@ -138,12 +138,12 @@ if($do_action > 0){
 		}elseif($act_type == 8){
 			$lead = new Leadext($db);
 			$lead->fetch($do_action);
-			$lead->array_options["options_new"] = 0;
+			$lead->array_options["options_new"] = 1;
 			$lead->update($user);
 		}elseif($act_type == 9){
 			$lead = new Leadext($db);
 			$lead->fetch($do_action);
-			$lead->array_options["options_new"] = 1;
+			$lead->array_options["options_new"] = 0;
 			$lead->update($user);
 		}
 	}
