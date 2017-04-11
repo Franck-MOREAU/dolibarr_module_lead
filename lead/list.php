7b454288a19712db8a7d2adfde41a5c7ee80bafa
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-04-11 15:03:07 +0200

    debug

--- lead/list.php
+++ lead/list.php
@@ -317,7 +317,7 @@ if ($resql != - 1) {
 	$i = 0;
 	print '<table class="noborder" width="100%">';
 	print '<tr class="liste_titre">';
-	print '<td align="center" width="30px">Action</td>';
+	print '<td align="center" width="60px">Action</td>';
 	print_liste_field_titre($langs->trans("Ref"), $_SERVEUR['PHP_SELF'], "t.ref", "", $option, '', $sortfield, $sortorder);
 	print_liste_field_titre($langs->trans("LeadCommercial"), $_SERVEUR['PHP_SELF'], "usr.lastname", "", $option, '', $sortfield, $sortorder);
 	print_liste_field_titre($langs->trans("Customer"), $_SERVEUR['PHP_SELF'], "so.nom", "", $option, '', $sortfield, $sortorder);
