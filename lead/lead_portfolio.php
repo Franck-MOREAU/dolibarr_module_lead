459be132a591a02b184553e640d5bdbf4c64ee9e
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-04-12 16:56:49 +0200

    debug

--- lead/lead_portfolio.php
+++ lead/lead_portfolio.php
@@ -95,7 +95,7 @@ $filter4['t.fk_c_status !IN'] = '6,5,11';
 $filter5['t.fk_c_status !IN'] = '6,5,7';
 $filter1['leadextra.chaude'] =1;
 $filter2['leadextra.chaude IS NULL'] =1;
-// $resql1 = $object1->fetch_all('DESC', 't.ref','' , 0, $filter1);
+$resql1 = $object1->fetch_all('DESC', 't.ref','' , 0, $filter1);
 // $resql2 = $object2->fetch_all('DESC', 't.ref','' , 0, $filter2);
 // $resql3 = $object3->fetch_all('DESC', 't.ref','' , 0, $filter3);
 // $resql4 = $object4->fetch_all('DESC', 't.ref','' , 0, $filter4);
@@ -162,8 +162,50 @@ print '<tr style="height:140px;">';
 
 //affaires en cours chaude
 print '<td class="colone" rowspan="3" style="height:540px;"><div id="encours_chaude_1" class="dropper" ondrop="drop(event)" ondragover="allowDrop(event)" style="height:540px; width:219px; overflow: auto;">';
-print '</div></td>';
-print '<td class="colone" rowspan="3" style="height:540px;"><div id="encours_chaude_2" class="dropper" ondrop="drop(event)" ondragover="allowDrop(event)" style="height:540px; width:219px; overflow: auto;">';
+$i=0;
+foreach ($object1->lines as $line){
+	$line->fetch_thirdparty();
+	if($line->array_options['options_type']==1){
+		$img = img_picto('porteur', 'reception.png@volvo');
+	}elseif($line->array_options['options_type']==2){
+		$img = img_picto('porteur', 'tracteur.png@volvo');
+	}
+	if($line->array_options['options_gamme'] == 1){
+		$color = '#56ff56';
+		$color2= '#00ff00';
+	}elseif($line->array_options['options_gamme'] == 2){
+		$color = '#ff5656';
+		$color2= '#ff0000';
+	}elseif($line->array_options['options_gamme'] == 18){
+		$color = '#ffaa56';
+		$color2= '#ff7f00';
+	}elseif($line->array_options['options_gamme'] == 3){
+		$color = '#aad4ff';
+		$color2= '#56aaff';
+	}elseif($line->array_options['options_gamme'] == 4){
+		$color = '#aa56ff';
+		$color2= '#7f00ff';
+	}else{
+		$color = '#cccccc';
+		$color2= '#b2b2b2';
+	}
+	print'<div class="cal_event cal_event_busy"  draggable="true" ondragstart="drag(event,this)" id="'. $line->id . '" style="background: -webkit-gradient(linear, left top, left bottom, from('.$color.'), to('.$color2.'));';
+	print 'border-radius:6px; margin-bottom: 3px; width:200px;">';
+	print $img . ' ';
+	print $line->ref . '</br>';
+	print $line->thirdparty->name;
+	print '</div>';
+	$i++;
+	if ($i>= $mid1|| $mid1 == 0){
+		print '</div></td>';
+		print '<td class="colone" rowspan="3" style="height:540px;"><div id="encours_chaude_2" class="dropper" ondrop="drop(event)" ondragover="allowDrop(event)" style="height:540px; width:219px; overflow: auto;">';
+		$i =-1*$i;
+	}
+}
+if($resql1==0){
+	print '</div></td>';
+	print '<td class="colone" rowspan="3" style="height:540px;"><div id="encours_chaude_2" class="dropper" ondrop="drop(event)" ondragover="allowDrop(event)" style="height:540px; width:219px; overflow: auto;">';
+}
 print '</div></td>';
 
 //affaire traitée modifiables
