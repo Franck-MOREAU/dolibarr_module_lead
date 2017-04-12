ffe043d3ef29a1e31da5e0fc5d10b53d8765ad29
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-04-12 17:39:57 +0200

    debug

--- lead/dragdrop.php
+++ lead/dragdrop.php
@@ -6,7 +6,14 @@ if (! $res)
 if (! $res)
 	die("Include of main fails");
 
+global $db;
+require_once DOL_DOCUMENT_ROOT . '/volvo/class/lead.extend.class.php';
+
 $lead_id = GETPOST('id_lead');
 $new_statut = GETPOST('new_statut');
-echo 'l\'affaire N°' . $lead_id . ' prend le statut ' . $new_statut;
+
+$lead = new Leadext($db);
+$res = $lead->fetch($lead_id);
+
+echo 'l\'affaire N°' . $lead_id . ' prend le statut ' . $new_statut . ' la recherche de l\'affaire a retournée le résultat: ' . $res;
 ?>
