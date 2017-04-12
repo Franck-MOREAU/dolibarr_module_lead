07929821bbe42bedf28204cb5450f0b108219510
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-04-12 11:03:41 +0200

    debug

--- lead/lead_portfolio.php
+++ lead/lead_portfolio.php
@@ -132,7 +132,7 @@ if ($resql != - 1) {
 		}
 		// ondrop="dropnot(event,this)"
 		print'<div class="cal_event cal_event_busy" style="background: -webkit-gradient(linear, left top, left bottom, from('.$color.'), to('.$color2.'));';
-		print ' -moz-border-radius:4px; width:25%;" id="'. $line->id . '" draggable="true" ondragstart="drag(event,this)" ondragover="false">';
+		print ' -moz-border-radius:4px; width:50%;" id="'. $line->id . '" draggable="true" ondragstart="drag(event,this)" ondragover="false">';
 		print $img . ' ';
 		print $line->ref . '</br>';
 		print $line->thirdparty->name;
