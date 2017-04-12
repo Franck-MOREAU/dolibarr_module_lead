0d13f6cb2d614196e4ed28e0ce6c38f01159e7d5
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-04-12 17:01:30 +0200

    debug

--- lead/lead_portfolio.php
+++ lead/lead_portfolio.php
@@ -298,8 +298,58 @@ print '<tr style="height:380px;">';
 
 //changement de ligne --> affaires traitées non modifiables--> data
 print '<td class="colone" rowspan="3" style="height:540px;"><div id="traite_fige_1" class="dropper" ondrop="drop(event)" ondragover="allowDrop(event)" style="height:540px; width:219px; overflow: auto;">';
-print '</div></td>';
-print '<td class="colone" rowspan="3" style="height:540px;"><div id="traite_fige_1" class="dropper" ondrop="drop(event)" ondragover="allowDrop(event)" style="height:540px; width:219px; overflow: auto;">';
+$i=0;
+foreach ($object5->lines as $line){
+	$line->fetch_thirdparty();
+	if($line->array_options['options_type']==1){
+		$img = img_picto('porteur', 'reception.png@volvo');
+	}elseif($line->array_options['options_type']==2){
+		$img = img_picto('porteur', 'tracteur.png@volvo');
+	}
+	if($line->array_options['options_gamme'] == 1){
+		$color = '#56ff56';
+		$color2= '#00ff00';
+	}elseif($line->array_options['options_gamme'] == 2){
+		$color = '#ff5656';
+		$color2= '#ff0000';
+	}elseif($line->array_options['options_gamme'] == 18){
+		$color = '#ffaa56';
+		$color2= '#ff7f00';
+	}elseif($line->array_options['options_gamme'] == 3){
+		$color = '#aad4ff';
+		$color2= '#56aaff';
+	}elseif($line->array_options['options_gamme'] == 4){
+		$color = '#aa56ff';
+		$color2= '#7f00ff';
+	}else{
+		$color = '#cccccc';
+		$color2= '#b2b2b2';
+	}
+	$lead = new Leadext($db);
+	$lead->fetch($line->id);
+	$cmd = $lead->getnbchassisreal();
+	if($cmd>0){
+		$drag = "false";
+	}else{
+		$drag = "true";
+	}
+	print'<div class="cal_event cal_event_busy"  draggable="'. $drag . '" ondragstart="drag(event,this)" id="'. $line->id . '" style="background: -webkit-gradient(linear, left top, left bottom, from('.$color.'), to('.$color2.'));';
+	print 'border-radius:6px; margin-bottom: 3px; width:200px;">';
+	print $img . ' ';
+	print $line->ref . '</br>';
+	print $line->thirdparty->name;
+	print '</div>';
+	$i++;
+	if ($i>= $mid3){
+		print '</div></td>';
+		print '<td class="colone" rowspan="3" style="height:540px;"><div id="traite_fige_1" class="dropper" ondrop="drop(event)" ondragover="allowDrop(event)" style="height:540px; width:219px; overflow: auto;">';
+		$i =-1*$i;
+	}
+}
+if($resql3 == 0){
+	print '</div></td>';
+	print '<td class="colone" rowspan="3" style="height:540px;"><div id="traite_fige_1" class="dropper" ondrop="drop(event)" ondragover="allowDrop(event)" style="height:540px; width:219px; overflow: auto;">';
+}
 print '</div></td>';
 
 //changement de ligne --> affaires en cours froides--> titre
