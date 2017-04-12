2c42dc92265481ef40e6e08320b9a16e58b230fa
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-04-12 17:56:45 +0200

    debug

--- lead/dragdrop.php
+++ lead/dragdrop.php
@@ -6,7 +6,7 @@ if (! $res)
 if (! $res)
 	die("Include of main fails");
 
-global $db;
+global $db, $user;
 require_once DOL_DOCUMENT_ROOT . '/volvo/class/lead.extend.class.php';
 
 $lead_id = GETPOST('id_lead');
@@ -14,6 +14,40 @@ $new_statut = GETPOST('new_statut');
 
 $lead = new Leadext($db);
 $res = $lead->fetch($lead_id);
+if($res>0){
+	$statut = explode('_', $new_statut);
+	switch($statut[0]){
+		case 'encours':
+			$c_status= 5;
+			switch($statut[1]){
+				case 'chaude':
+					$chaude =1;
+					break;
+				case 'froide':
+					$chaude =0;
+					break;
+			}
+			break;
+		case 'traite':
+			$_c_status = 6;
+			$chaude=0;
+			break;
 
-echo 'l\'affaire N°' . $lead_id . ' prend le statut ' . $new_statut . ' la recherche de l\'affaire a retournée le résultat: ' . $res;
+		case 'perdu':
+			$c_status=7;
+			$chaude=0;
+			break;
+
+		case 'sanssuite':
+			$c_status = 11;
+			$chaude = 0;
+			break;
+	}
+
+	$lead->fk_c_status = $c_statut;
+	$lead->array_options['options_chaude'] = $chaude;
+	$lead->update($user);
+}else{
+	echo $lead->error;
+}
 ?>
