70c1a6b11f1987a4b59db36ec8701cb2e2727a2d
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-04-13 11:38:32 +0200

    debug

--- lead/lead_portfolio.php
+++ lead/lead_portfolio.php
@@ -127,7 +127,8 @@ print '}';
 
 
 print 'function drag(ev) {';
-print 'ev.dataTransfer.setData("text/plain", ev.target.id);';
+//print 'ev.dataTransfer.setData("text/plain", ev.target.id);';
+print 'ev.dataTransfer.add(ev.target.id,"text/plain");';
 // print 'var datalist = ev.DataTransferItemList;';
 // print 'datalist.add(ev.target.id,"text/plain"); ';
 print '}';
