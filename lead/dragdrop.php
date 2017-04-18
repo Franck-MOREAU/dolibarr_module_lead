2761a3f35dd999cf530c4f123e628967d4fbfd38
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-04-18 10:35:02 +0200

    debug

--- lead/dragdrop.php
+++ lead/dragdrop.php
@@ -17,43 +17,49 @@ $form = new Form($db);
 $lead = new Leadext($db);
 $res = $lead->fetch($lead_id);
 if($res>0){
-	$statut = explode('_', $new_statut);
-	switch($statut[0]){
-		case 'encours':
-			$c_status= 5;
-			switch($statut[1]){
-				case 'chaude':
-					$chaude =1;
-					break;
-				case 'froide':
-					$chaude =0;
-					break;
-			}
-			break;
-		case 'traite':
-			$c_status = 6;
-			$chaude=0;
-			break;
+	if(empty($action)){
+		$statut = explode('_', $new_statut);
+		switch($statut[0]){
+			case 'encours':
+				$c_status= 5;
+				switch($statut[1]){
+					case 'chaude':
+						$chaude =1;
+						break;
+					case 'froide':
+						$chaude =0;
+						break;
+				}
+				break;
+			case 'traite':
+				$c_status = 6;
+				$chaude=0;
+				break;
 
-		case 'perdu':
-			$formconfirm = '';
-			$formconfirm = $form->formconfirm($_SERVER["PHP_SELF"] . '?id_lead=' . $lead_id . '&$new_statut=' . $new_statut, 'test1', 'test2', 'confirm_move', '', 0, 1);
-			print $formconfirm;
-			$c_status=7;
-			$chaude=0;
-			break;
+			case 'perdu':
+				$formconfirm = '';
+				$formconfirm = $form->formconfirm($_SERVER["PHP_SELF"] . '?id_lead=' . $lead_id . '&$new_statut=' . $new_statut, 'test1', 'test2', 'confirm_move', '', 0, 1);
+				break;
 
-		case 'sanssuite':
-			$c_status = 11;
-			$chaude = 0;
-			break;
+			case 'sanssuite':
+				$c_status = 11;
+				$chaude = 0;
+				break;
+		}
+
+		$lead->fk_c_status = $c_status;
+		$lead->array_options['options_chaude'] = $chaude;
+		$res = $lead->update($user);
+		echo $lead->error;
+	}else{
+		echo $lead->error;
 	}
-
+}elseif($action=='confirm_move'){
+	$c_status=7;
+	$chaude=0;
 	$lead->fk_c_status = $c_status;
 	$lead->array_options['options_chaude'] = $chaude;
 	$res = $lead->update($user);
-	echo $lead->error;
-}else{
 	echo $lead->error;
 }
 ?>
